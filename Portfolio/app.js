69dd6939745837ac5de0e19ec3807103e739d0ac
AlkaDwivedi <dalka8960@gmail.com>
2024-09-01 21:25:30 +0530

    Add files via upload

---
+++ Portfolio/app.js
@@ -0,0 +1,13 @@
+// Smooth Scrolling for Navigation
+document.querySelectorAll('nav ul li a, .cta-btn').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        if (targetElement) {
+            targetElement.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
+});
